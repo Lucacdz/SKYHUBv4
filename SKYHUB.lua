5e6461a4377a6e5a41dbea3d62b49910cb26b88e
Lucacdz <duyanh4416t@gmail.com>
2025-08-05 13:42:47 +0700

    Update SKYHUB.lua

--- SKYHUB.lua
+++ SKYHUB.lua
@@ -145,7 +145,7 @@ iconStroke.Thickness = 2
 
 -- Main Frame - Tự động điều chỉnh kích thước
 local mainFrame = Instance.new("Frame")
-mainFrame.Size = UDim2.new(0, 400, 0, 200)
+mainFrame.Size = UDim2.new(0,200, 0, 200)
 mainFrame.Position = UDim2.new(0.5, -175, 0.5, -100)
 mainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
 mainFrame.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
